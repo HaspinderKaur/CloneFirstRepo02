8e6ea7520141b26bfea4cdae7ff0befc2f1ce172
Haspinder Dhillon <kauhaspi@sheridancollege.ca>
2026-06-01 20:42:37 -0400

    Create a multiplication program

---
+++ CloneWorkFlow02/src/main/java/com/mycompany/cloneworkflow02/CloneWorkFlow02.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cloneworkflow02;
+
+/**
+ *
+ * @author Haspinder Dhillon, 2026
+ */
+public class CloneWorkFlow02 {
+
+    public static void main(String[] args) {
+        
+        int num1 = 15; 
+        int num2 = 17; 
+        int multi = num1 * num2; 
+        System.out.println("First Number: " + num1); 
+        System.out.println("Second Number: " + num2); 
+        System.out.println("Answer is: " + multi);
+    }
+}
